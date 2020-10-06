ecd70487b7398e33a751bdece9e5e6794a58210d
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-06 11:54:00 +0300

    spot checks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react"
+import axios from "axios"
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      users: [],
+    }
+  }
+
+  async getUsers() {
+    return axios.get("https://jsonplaceholder.typicode.com/users")
+  }
+
+  async componentDidMount() {
+    const response = await this.getUsers()
+    this.setState({ users: response.data })
+  }
 
   render() {
+    console.log(this.state.users)
     return (
-      <div className="App">
-        <h1>Hello, World!</h1>
+      <div className="App" id="people">
+        {this.state.users.map((u) => {
+          return <User key={u.id} userData={u} />
+        })}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+class User extends Component {
+  render() {
+    const user = this.props.userData
+    return (
+      <div className="person">
+        <div className="name">{user.name}</div>
+        <div>{user.email}</div>
+      </div>
+    )
+  }
+}
+
+export default App
